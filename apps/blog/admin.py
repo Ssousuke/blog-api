782a4769adf58e47743bcdd8655e0be8f1204d75
Wesley Farias <wesleysousukee@gmail.com>
2022-03-28 03:43:03 -0300

    adiciona model e admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,40 @@
+from csv import list_dialects
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'description',
+        'created_at',
+        'updated_at',
+        'published'
+    ]
+    list_display_links = [
+        'title',
+        'description',
+        'created_at',
+        'updated_at',
+    ]
+    list_editable = [
+        'published'
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'created_at',
+        'updated_at',
+    ]
+    list_display_links = [
+        'id',
+        'name',
+        'created_at',
+        'updated_at',
+    ]
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=80)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['created_at']
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    body = models.TextField()
+    thumb = models.ImageField(upload_to='post/thumb/', blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['created_at']
