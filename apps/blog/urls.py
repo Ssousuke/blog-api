3376de8e3921fdca34a67ff63b70e141d469319b
Wesley Farias <wesleysousukee@gmail.com>
2022-03-30 05:43:16 -0300

    implementa get, post, delete, detail da view de Post

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .import views
+from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('list/', views.BlogPostApiView.as_view(), name='list_post'),
+    path('', views.BlogListPosts.as_view(), name='list_post'),
+    path('detail/<int:pk>/', views.BlogGetPost.as_view(), name='get_post'),
+    path('new/<int:pk>/', views.BlogListPosts.as_view(), name='new_post'),
+    path('delete/<int:pk>/', views.BlogListPosts.as_view(), name='delete_post'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,8 +1,37 @@
-from rest_framework import generics
+from django.http import Http404
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Post, Category
 from .serializers import BlogPostSerializer, BlogCategorySerializer
 
 
-class BlogPostApiView(generics.ListCreateAPIView):
+class BlogGetPost(APIView):
+    @staticmethod
+    def get_object(pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = BlogPostSerializer(post)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BlogListPosts(generics.ListAPIView):
     queryset = Post.objects.filter(published=True)
     serializer_class = BlogPostSerializer
